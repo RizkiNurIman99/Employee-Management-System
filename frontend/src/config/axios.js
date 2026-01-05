a8276e8cdcd92796ae148b4a0bf983103c86d616
unknown <rizkinuriman7@gmail.com>
2026-01-05 14:34:27 +0700

    setting vps

--- frontend/src/config/axios.js
+++ frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:5000",
+  VITE_API_BASE_URL: "http://31.97.220.189:5000",
 });
 
 api.interceptors.request.use((config) => {
