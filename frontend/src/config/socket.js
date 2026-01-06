13c2f61c067b99afc4323888676167ae52c863dc
unknown <rizkinuriman7@gmail.com>
2026-01-06 11:01:59 +0700

    updated  config socket.io

--- frontend/src/config/socket.js
+++ frontend/src/config/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 const SOCKET_URL = import.meta.env.VITE_BASE_URL;
 
 const socket = io(SOCKET_URL, {
-  transports: ["websocket"],
+  transports: ["polling"],
   withCredentials: true,
 });
 
