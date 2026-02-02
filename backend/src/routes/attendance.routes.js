cf61dcc5932f64fbbf5cda86aaaac6c38adf4f8a
unknown <rizkinuriman7@gmail.com>
2026-02-02 15:24:55 +0700

    add limmiter, auth rfid access and delete route not use

--- backend/src/routes/attendance.routes.js
+++ backend/src/routes/attendance.routes.js
@@ -1,29 +1,21 @@
 import express from "express";
 import {
   attendance,
-  checkIn,
-  checkOut,
   deleteAttendanceRecord,
   getAllTodaysAttendance,
   getAttendanceEmployee,
   markStatus,
 } from "../controller/attendanceController.js";
-import {
-  getDailyReport,
-  getMonthlyReport,
-} from "../controller/attendanceReport.js";
+import { getDailyReport } from "../controller/attendanceReport.js";
 
 const router = express.Router();
 
 router.post("/attendance", attendance);
-router.post("/attendance/manual-checkIn", checkIn);
-router.put("/attendance/manual-checkOut", checkOut);
 router.post("/attendance/mark-status", markStatus);
 router.get("/attendance/getAttendance", getAttendanceEmployee);
 router.get("/attendance/attendances", getAllTodaysAttendance);
 router.delete("/attendance/delete-record/:id", deleteAttendanceRecord);
 
 router.get("/attendance-report/daily-report", getDailyReport);
-router.get("/attendance-report/daily-monthly", getMonthlyReport);
 
 export default router;
--- backend/src/routes/rfid.Routes.js
+++ backend/src/routes/rfid.Routes.js
@@ -1,8 +1,10 @@
 import express from "express";
 import { rfidScan } from "../controller/rfidScan.js";
+import { rfidAuth } from "../auth/rfidAuth.js";
+import { rfidLimiter } from "../config/rateLimmiter.js";
 
 const router = express.Router();
 
-router.post("/rfid-scan", rfidScan);
+router.post("/rfid-scan", rfidAuth, rfidLimiter, rfidScan);
 
 export default router;
