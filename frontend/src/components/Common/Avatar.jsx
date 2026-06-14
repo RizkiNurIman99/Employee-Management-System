6ebcd0e7c866b055cdfe2933705cd6dec8ec361d
unknown <rizkinuriman7@gmail.com>
2026-06-14 20:43:05 +0700

    Add new component Avatar

---
+++ frontend/src/components/Common/Avatar.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const IMAGE_BASE_URL = import.meta.env.VITE_IMAGE_BASE_URL;
+const DEFAULT_AVATAR = `${IMAGE_BASE_URL}/avatar/default-avatar.png`;
+
+const Avatar = ({ src, alt = "Avatar", className = "" }) => {
+  const [imgSrc, setImageSrc] = useState(
+    src ? `${IMAGE_BASE_URL}/avatar/${src}` : DEFAULT_AVATAR,
+  );
+  return (
+    <img
+      src={imgSrc}
+      alt={alt}
+      className={className}
+      onError={() => {
+        if (imgSrc !== DEFAULT_AVATAR) {
+          setImageSrc(DEFAULT_AVATAR);
+        }
+      }}
+    />
+  );
+};
+
+export default Avatar;
